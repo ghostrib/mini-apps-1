0c9b671e65860870e2e25245c7b69b4239723a32
gizmo <gizmo@Gremlin.local>
2018-12-10 13:57:48 -0600

    working tic-tac-toe board

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,27 @@
+window.onload = function () {
 
+    let turn = 'X';
+    let cells = document.getElementById("board").getElementsByTagName("td");
+    let btn = document.getElementsByTagName('button')[0];
 
+    function next() {
+        turn = turn === 'X' ? 'O' : 'X';
+    }
+    function play() {
+        if (this.innerHTML === '') {
+            this.innerHTML = turn;
+            next();
+        }
+    }
+
+    for (var i in cells) {
+        cells[i].onclick = play;
+    }
+
+    btn.onclick = function () {
+        for (var i in cells) {
+            cells[i].innerHTML = '';
+        }
+    }
+
+}
