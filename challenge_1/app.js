c8349ed5c7f004f0606c646168bbb95ed5c43261
gizmo <gizmo@Gremlin.local>
2018-12-10 15:21:22 -0600

    refactored using const and let

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,10 +1,10 @@
 window.onload = function () {
 
     let turn = 'X';
-    let cells = document.getElementById("board").getElementsByTagName("td");
+    let board = document.getElementById("board").getElementsByTagName("td");
     let btn = document.getElementsByTagName('button')[0];
 
-    function play() {
+    const play = function () {
         if (this.innerHTML === '') {
             this.innerHTML = turn;
             setTimeout(function () {
@@ -12,67 +12,53 @@ window.onload = function () {
             }, 0)
             turn = turn === 'X' ? 'O' : 'X';
         }
-
     }
 
-    function checkWinner() {
+    const resetGame = function () {
+        turn = 'X';
+        for (var i in board) {
+            board[i].innerHTML = ''
+        }
+    }
+
+    const checkWinner = function () {
         let winner;
-        let board = document.getElementById('board').getElementsByTagName('td');
         if (board[0].innerHTML === board[1].innerHTML && board[2].innerHTML === board[0].innerHTML && board[0].innerHTML !== '') {
             winner = board[0].innerHTML;
-            //console.log(board[0].innerHTML, 'wins')
         }
-
         if (board[3].innerHTML === board[4].innerHTML && board[5].innerHTML === board[3].innerHTML && board[3].innerHTML !== '') {
             winner = board[3].innerHTML;
-            //console.log(board[3].innerHTML, 'wins')
         }
-
         if (board[6].innerHTML === board[7].innerHTML && board[8].innerHTML === board[6].innerHTML && board[6].innerHTML !== '') {
             winner = board[6].innerHTML;
-            //console.log(board[6].innerHTML, 'wins')
         }
-
         if (board[0].innerHTML === board[3].innerHTML && board[6].innerHTML === board[0].innerHTML && board[0].innerHTML !== '') {
             winner = board[0].innerHTML;
-            //console.log(board[0].innerHTML, 'wins')
         }
-
         if (board[1].innerHTML === board[4].innerHTML && board[7].innerHTML === board[1].innerHTML && board[1].innerHTML !== '') {
             winner = board[1].innerHTML;
-            //console.log(board[1].innerHTML, 'wins')
         }
-
         if (board[2].innerHTML === board[5].innerHTML && board[8].innerHTML === board[2].innerHTML && board[2].innerHTML !== '') {
             winner = board[2].innerHTML;
-            //console.log(board[2].innerHTML, 'wins')
         }
-
         if (board[0].innerHTML === board[4].innerHTML && board[8].innerHTML === board[0].innerHTML && board[0].innerHTML !== '') {
             winner = board[0].innerHTML;
-            //console.log(board[0].innerHTML, 'wins')
         }
-
         if (board[2].innerHTML === board[4].innerHTML && board[6].innerHTML === board[2].innerHTML && board[2].innerHTML !== '') {
             winner = board[2].innerHTML;
-            //console.log(board[2].innerHTML, 'wins')
         }
         if (winner) {
             alert(winner + ' wins!')
-            for (var i in cells) {
-                cells[i].innerHTML = '';
-            }
+            resetGame();
         }
     }
 
-    for (var i in cells) {
-        cells[i].onclick = play;
+    for (let i in board) {
+        board[i].onclick = play;
     }
 
     btn.onclick = function () {
         turn = 'X';
-        for (var i in cells) {
-            cells[i].innerHTML = '';
-        }
+        resetGame();
     }
 }
